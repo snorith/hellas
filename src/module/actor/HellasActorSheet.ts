30d98643287dcd5983fa4748ba2b589794bd0c08
Stephen Smith <stephen@norith.com>
2021-01-30 13:21:10 -0500

    working background image behind fate points

--- src/module/actor/HellasActorSheet.ts
+++ src/module/actor/HellasActorSheet.ts
@@ -72,9 +72,9 @@ export class HellasActorSheet extends ActorSheet {
 		// Everything below here is only needed if the sheet is editable
 		if (!this.options.editable) return;
 
-		// // Add Inventory Item
-		// html.find('.item-create').click(this._onItemCreate.bind(this));
-		//
+		// Add Inventory Item
+		html.find('.item-create').click(this._onItemCreate.bind(this));
+
 		// // Update Inventory Item
 		// html.find('.item-edit').click(ev => {
 		// 	const td = $(ev.currentTarget).parents(".item");
@@ -95,6 +95,10 @@ export class HellasActorSheet extends ActorSheet {
 		html.find('.fate-progress').click(this._onFateProgressClick.bind(this))
 	}
 
+	/**
+	 * Handle adding or removing fate points from the character sheet
+	 * @param event
+	 */
 	_onFateProgressClick(event) {
 		event.preventDefault();
 
@@ -119,5 +123,33 @@ export class HellasActorSheet extends ActorSheet {
 		this.render(false)
 
 		return false
+	}
+
+	/**
+	 * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+	 * @param {Event} event   The originating click event
+	 * @private
+	 */
+	_onItemCreate(event) {
+		event.preventDefault();
+
+		const element = event.currentTarget;
+		// Get the type of item to create.
+		const type = element.dataset.type;
+		// Grab any data associated with this control.
+		const data = duplicate(element.dataset);
+		// Initialize a default name.
+		const name = `New ${type.capitalize()}`;
+		// Prepare the item object.
+		const itemData = {
+			name: name,
+			type: type,
+			data: data
+		};
+		// Remove the type from the dataset since it's in the itemData.type prop.
+		delete itemData.data["type"];
+
+		// Finally, create the item!
+		return this.actor.createOwnedItem(itemData);
 	}
 }
